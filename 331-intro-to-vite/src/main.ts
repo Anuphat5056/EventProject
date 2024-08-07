ed5d181727692d81852a85504b298a29582fdcbd
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-08-07 22:33:23 +0700

    edit inject

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -7,11 +7,10 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import 'nprogress/nprogress.css';
 import router from './router';
-import { inject } from '@vercel/analytics'
 
-inject()
 const app = createApp(App)
-
+import { inject } from '@vercel/analytics'
+inject()
 app.use(createPinia())
 app.use(router)
 
