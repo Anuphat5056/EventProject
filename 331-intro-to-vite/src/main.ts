49c06c81e54d06a0a50bb9384517ead902b11ae6
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-08-07 23:27:49 +0700

    update main

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -3,6 +3,7 @@
 import './assets/style.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue';
 import 'nprogress/nprogress.css';
@@ -10,6 +11,7 @@ import router from './router';
 
 const app = createApp(App)
 
+inject()
 app.use(createPinia())
 app.use(router)
 
