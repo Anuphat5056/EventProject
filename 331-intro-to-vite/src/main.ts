b812f98514b69890d05a380397d4aee671814a86
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-08-07 23:25:19 +0700

    rgwgwg

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -1,19 +1,16 @@
 // import './assets/main.css'
 
-import './assets/style.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import './assets/style.css';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import 'nprogress/nprogress.css'
-import { createAppRouter } from './router'
-import { inject } from '@vercel/analytics'
+import App from './App.vue';
+import 'nprogress/nprogress.css';
+import router from './router';
 
 const app = createApp(App)
-const pageLimit = [null, 1, 1, 2, 2] 
 
-inject()
 app.use(createPinia())
-app.use(createAppRouter(pageLimit))
+app.use(router)
 
 app.mount('#app')
