4a2f2def491a4bd4e552a67ccdb46152ad0b54b3
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-08-07 23:37:52 +0700

    change so can deploy

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -3,7 +3,6 @@
 import './assets/style.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { inject } from '@vercel/analytics'
 
 import App from './App.vue';
 import 'nprogress/nprogress.css';
@@ -11,7 +10,6 @@ import router from './router';
 
 const app = createApp(App)
 
-inject()
 app.use(createPinia())
 app.use(router)
 
